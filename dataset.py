b22ac6e8d3cfa1769a776eac970d5bf4aeb208b5
FogadOg <fogadm06@gmail.com>
2024-06-24 21:18:31 +0200

    refactoring

--- dataset.py
+++ dataset.py
@@ -19,22 +19,21 @@ class Dataset():
             dataPoints = []
 
             imageName = image.find('imageName').text
-            sample["image"] = self.normalize(self.folderName + imageName)
-
             resolution = image.find('resolution')
-            resolutionX = resolution.attrib['x']
-            resolutionY = resolution.attrib['y']
+            originalWidth = int(resolution.attrib['x'])
+            originalHeight = int(resolution.attrib['y'])
+            sample["image"], scale_x, scale_y = self.normalize(self.folderName + imageName, originalWidth, originalHeight)
 
-            sample["resolutionX"] = resolutionX
-            sample["resolutionY"] = resolutionY
+            sample["resolutionX"] = self.targetSize[0]
+            sample["resolutionY"] = self.targetSize[1]
 
             taggedRectangles = image.find('taggedRectangles')
             for rect in taggedRectangles.findall('taggedRectangle'):
                 dataPoint = dict()
-                dataPoint["x"] = float(rect.attrib['x'])
-                dataPoint["y"] = float(rect.attrib['y'])
-                dataPoint["width"] = float(rect.attrib['width'])
-                dataPoint["height"] = float(rect.attrib['height'])
+                dataPoint["x"] = float(rect.attrib['x']) * scale_x
+                dataPoint["y"] = float(rect.attrib['y']) * scale_y
+                dataPoint["width"] = float(rect.attrib['width']) * scale_x
+                dataPoint["height"] = float(rect.attrib['height']) * scale_y
                 dataPoint["offset"] = float(rect.attrib['offset'])
                 dataPoint["rotation"] = float(rect.attrib['rotation'])
                 dataPoint["userName"] = rect.attrib['userName']
@@ -48,13 +47,15 @@ class Dataset():
             sample["dataPoints"] = dataPoints
             self.dataset.append(sample)
     
-    def normalize(self, filePath: str):        
+    def normalize(self, filePath: str, originalWidth: int, originalHeight: int):        
         cvImage = cv2.imread(filePath)
         cvImage = cv2.cvtColor(cvImage, cv2.COLOR_BGR2RGB)
         cvImage = cv2.resize(cvImage, self.targetSize)
+        scale_x = self.targetSize[0] / originalWidth
+        scale_y = self.targetSize[1] / originalHeight
         cvImage = torch.tensor(cvImage, dtype=torch.float32).permute(2, 0, 1) / 255.0
 
-        return cvImage
+        return cvImage, scale_x, scale_y
     
     def __len__(self):
         return len(self.dataset)
@@ -63,5 +64,7 @@ class Dataset():
         return self.dataset[idx]["image"], self.dataset[idx]["dataPoints"]
 
 if __name__ == "__main__":
+    from plot import Plot
+    plot = Plot()
     dataset = Dataset('SceneTrialTest/words.xml')
-    print(dataset[0])
+    plot.renderRectangles(dataset.dataset[0])
