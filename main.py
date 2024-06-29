a049c791a6accf5dea08c45afe46552b285a8b85
FogadOg <fogadm06@gmail.com>
2024-06-29 21:10:17 +0200

    removing scheduler

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ print(device)
 dataset = Dataset('SceneTrialTest/words.xml')
 vocabSize = len(dataset.tokenizer)
 
-model = Model(vocabSize)
+model = Model(vocabSize, 30)
 
 dataloader = Dataloader(dataset, .8)
 trainDataloader, testDataloader = dataloader.splitDataset()
@@ -19,10 +19,10 @@ trainDataloader, testDataloader = dataloader.splitDataset()
 
 optimizer = torch.optim.Adam(model.parameters(), lr=.001)
 
-from torch.optim.lr_scheduler import ReduceLROnPlateau
-scheduler = ReduceLROnPlateau(optimizer, mode='min', factor=0.1, patience=20, verbose=True)
+# from torch.optim.lr_scheduler import ReduceLROnPlateau
+# scheduler = ReduceLROnPlateau(optimizer, mode='min', factor=0.1, patience=20, verbose=True)
 
 criterionBbox = nn.SmoothL1Loss()
 
 
-Trainer(trainDataloader, testDataloader, model, criterionBbox, optimizer, 6000, device)
+Trainer(trainDataloader, testDataloader, model, criterionBbox, optimizer, 6000, device, dataset.tokenizer)
